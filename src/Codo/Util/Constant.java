c050c0ae48175661436e12450cb6982693bdf14c
bazingaterry <terry@32ph.com>
2016-11-23 16:55:02 +0800

    add Constant

---
+++ src/Codo/Util/Constant.java
@@ -0,0 +1,16 @@
+package Codo.Util;
+
+/**
+ * Created by terrychan on 23/11/2016.
+ */
+public interface Constant {
+    String SALT = "https://itun.es/hk/zwLMW?i=815423920";
+
+    String DB_HOST = "192.168.1.124";
+    String DB_PORT = "3306";
+    String DB_NAME = "Codo";
+
+    String TB_USER = "user";
+    String TB_TOKEN = "token";
+
+}
