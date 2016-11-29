0ba3ac8121683109b0679e28cbc8f71d2dd996dd
bazingaterry <terry@32ph.com>
2016-11-30 03:05:42 +0800

    update sql

--- database/new_database.sql
+++ database/new_database.sql
@@ -1,37 +1,21 @@
--- Create syntax for TABLE 'user'
-CREATE TABLE `user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL DEFAULT '',
-  `password` varchar(64) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `USERNAME` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
--- Create syntax for TABLE 'token'
-CREATE TABLE `token` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL,
-  `token` varchar(64) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `user_token` (`user_id`),
-  CONSTRAINT `token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8;
 -- Create syntax for TABLE 'channel'
 CREATE TABLE `channel` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL DEFAULT '',
+  `name` varchar(32) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `creater_id` int(11) unsigned NOT NULL,
   `last_update` timestamp NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   KEY `user_id` (`creater_id`),
-  CONSTRAINT `channel_ibfk_1` FOREIGN KEY (`creater_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8;
+  CONSTRAINT `user_id` FOREIGN KEY (`creater_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=136 DEFAULT CHARSET=utf8mb4;
+
 -- Create syntax for TABLE 'reminder'
 CREATE TABLE `reminder` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(64) NOT NULL DEFAULT '',
+  `title` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `creater_id` int(11) unsigned NOT NULL,
-  `content` text,
+  `content` text CHARACTER SET utf8,
   `type` int(11) NOT NULL,
   `due` timestamp NULL DEFAULT NULL,
   `priority` int(11) NOT NULL,
@@ -41,7 +25,27 @@ CREATE TABLE `reminder` (
   KEY `creater_id` (`creater_id`),
   CONSTRAINT `reminder_ibfk_1` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`id`),
   CONSTRAINT `reminder_ibfk_2` FOREIGN KEY (`creater_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4;
+
+-- Create syntax for TABLE 'token'
+CREATE TABLE `token` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `user_token` (`user_id`),
+  CONSTRAINT `token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4;
+
+-- Create syntax for TABLE 'user'
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `password` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `USERNAME` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4;
+
 -- Create syntax for TABLE 'user_channel'
 CREATE TABLE `user_channel` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -52,7 +56,8 @@ CREATE TABLE `user_channel` (
   KEY `channel_id` (`channel_id`),
   CONSTRAINT `user_channel_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `user_channel_ibfk_2` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=462 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=478 DEFAULT CHARSET=utf8mb4;
+
 -- Create syntax for TABLE 'user_reminder'
 CREATE TABLE `user_reminder` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -66,4 +71,4 @@ CREATE TABLE `user_reminder` (
   KEY `reminder_id` (`reminder_id`),
   CONSTRAINT `user_reminder_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `user_reminder_ibfk_2` FOREIGN KEY (`reminder_id`) REFERENCES `reminder` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4;
